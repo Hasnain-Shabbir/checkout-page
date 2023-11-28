b8c18e38aff630c1b93f401f2b7a3d6cd4555007
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-11-28 18:16:12 +0500

    create LabelInput component

---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as LabelInput } from '@/components/LabelInput';
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,37 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        xs: '480px',
+      },
+      colors: {
+        primary: {
+          100: '#F0F5FF',
+          200: '#1f8dd6',
+          300: '#1873b0',
+          400: '#0f5989',
+        },
+        'c-gray-100': '#f5f5f5',
+        'c-gray-200': '#ededed',
+        'c-gray-400': '#666666',
+      },
+      borderWidth: {
+        5: '5px',
+        6: '6px',
+        7: '7px',
+      },
+      minWidth: {
+        3: '12px',
+        4: '16px',
+        5: '20px',
+      },
+      height: {
+        4.5: '18px',
+      },
+      width: {
+        4.5: '18px',
       },
     },
   },
   plugins: [],
-}
+};
